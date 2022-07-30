49dd52c70885a1b91fbf3030e021d22fe91e91e9
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-31 04:16:38 +0900

    :sparkles: 引数からソースコードのパス文字列を取得する処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod arguments_handler;
+
 fn main() {
-    println!("Hello, world!");
+    let code_path = arguments_handler::get_code_path();
+
+    if let Ok(path) = code_path {
+        println!("{}", path);
+    }
 }
