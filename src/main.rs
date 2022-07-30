a3ce044df176ee85d015a51a143e76b29845ef73
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-31 05:47:37 +0900

    :sparkles: result.bfファイルを作ってそこに出力する処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod arguments_handler;
 mod code_reader;
+mod code_exporter;
 
 fn main() {
     let code_path = arguments_handler::get_code_path().unwrap();
     let code = code_reader::read_content(code_path).unwrap();
 
-    println!("{}", code);
+    code_exporter::export_code("result".to_string());
 }
