bbd26ee427535e5d4fb65a34f6aed827e59f83a0
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-07-31 05:13:11 +0900

    :sparkles: ソースファイルの中身を読み込む処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod arguments_handler;
+mod code_reader;
 
 fn main() {
-    let code_path = arguments_handler::get_code_path();
+    let code_path = arguments_handler::get_code_path().unwrap();
+    let code = code_reader::read_content(code_path).unwrap();
 
-    if let Ok(path) = code_path {
-        println!("{}", path);
-    }
+    println!("{}", code);
 }
