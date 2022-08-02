bec0dc854d403169a5ae63d9d6b5dac42d4ff1e5
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-08-03 02:58:33 +0900

    :sparkles: コードをトークンに分ける処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use code_generator::code_exporter;
+use crate::tokenizer::token_type::TokenType;
+use crate::tokenizer::tokenizer::tokenize;
 
 mod arguments_handler;
 mod code_reader;
@@ -8,5 +9,18 @@ fn main() {
     let code_path = arguments_handler::get_code_path().unwrap();
     let code = code_reader::read_content(code_path).unwrap();
 
-    code_exporter::export_code("result".to_string(), code);
+    let mut tokens = tokenize(code).unwrap();
+
+    for _ in 0..tokens.len() {
+        let token = tokens.pop_front();
+
+        if let Some(t) = token {
+            match t.token_type {
+                TokenType::Reserved(_) => println!("Reserved"),
+                TokenType::Number(n) => println!("Number: {}", n),
+            }
+        } else {
+            println!("Error");
+        }
+    }
 }
