bddf22d2b5b4494c6fda799ccf39abd5b82239de
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-08-02 20:22:38 +0900

    :zap: トークンの構造体のフィールドをpublicに変更

--- src/tokenizer/token.rs
+++ src/tokenizer/token.rs
@@ -1,6 +1,6 @@
 use crate::tokenizer::token_type::TokenType;
 
 pub struct Token {
-    token_type: TokenType,
-    next: Box<Option<Token>>,
+    pub token_type: TokenType,
+    pub next: Box<Option<Token>>,
 }
