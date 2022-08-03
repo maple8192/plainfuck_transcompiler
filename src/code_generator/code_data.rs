38756f9ba80cbe366a4d4e17d8c1f188a0370540
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-08-03 22:55:28 +0900

    :sparkles: CodeDataにメソッドを追加
    初期化とコマンド追加のメソッド

--- src/code_generator/code_data.rs
+++ src/code_generator/code_data.rs
@@ -4,3 +4,13 @@ use crate::code_generator::command::Command;
 pub struct CodeData {
     pub commands: VecDeque<Command>,
 }
+
+impl CodeData {
+    pub fn new() -> Self {
+        CodeData { commands: VecDeque::new() }
+    }
+
+    pub fn add_command(&mut self, command: Command) {
+        self.commands.push_back(command);
+    }
+}
