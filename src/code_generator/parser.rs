eb166c1dff6c9b2214fe4089b8c6f79e5856d25c
Maple8192 <57043077+maple8192@users.noreply.github.com>
2022-08-13 04:45:58 +0900

    :sparkles: パーサーを複数の文に対応

--- src/code_generator/parser.rs
+++ src/code_generator/parser.rs
@@ -1,3 +1,4 @@
+use crate::code_generator::parser::expression_queue::ExpressionQueue;
 use crate::code_generator::parser::node::binary_operator_type::BinaryOperatorType;
 use crate::code_generator::parser::node::Node;
 use crate::tokenizer::reserved_token::ReservedToken;
@@ -7,25 +8,35 @@ pub mod node;
 pub mod expression_queue;
 
 pub struct Parser {
-    expr: TokenQueue,
+    program: TokenQueue,
 }
 
 impl Parser {
-    pub fn new(expr: TokenQueue) -> Self {
-        Parser { expr }
+    pub fn new(tokens: TokenQueue) -> Self {
+        Parser { program: tokens }
     }
 
-    pub fn parse(&mut self) -> Node {
-        self.equality()
+    pub fn parse(&mut self) -> ExpressionQueue {
+        let mut expr_queue = ExpressionQueue::new();
+
+        while !self.program.is_end() {
+            expr_queue.add(self.equality());
+
+            if !self.program.consume_reserved_token(ReservedToken::EndStatement).unwrap() {
+                panic!("");
+            }
+        }
+
+        expr_queue
     }
 
     fn equality(&mut self) -> Node {
         let mut node = self.relational();
 
         loop {
-            if self.expr.consume_reserved_token(ReservedToken::Equal).unwrap() {
+            if self.program.consume_reserved_token(ReservedToken::Equal).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Equal, Box::new(node), Box::new(self.relational()));
-            } else if self.expr.consume_reserved_token(ReservedToken::NotEqual).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::NotEqual).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::NotEqual, Box::new(node), Box::new(self.relational()));
             } else {
                 return node;
@@ -37,13 +48,13 @@ impl Parser {
         let mut node = self.add();
 
         loop {
-            if self.expr.consume_reserved_token(ReservedToken::Less).unwrap() {
+            if self.program.consume_reserved_token(ReservedToken::Less).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Less, Box::new(node), Box::new(self.add()));
-            } else if self.expr.consume_reserved_token(ReservedToken::LessOrEqual).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::LessOrEqual).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::LessOrEqual, Box::new(node), Box::new(self.add()));
-            } else if self.expr.consume_reserved_token(ReservedToken::Greater).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::Greater).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Greater, Box::new(node), Box::new(self.add()));
-            } else if self.expr.consume_reserved_token(ReservedToken::GreaterOrEqual).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::GreaterOrEqual).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::GreaterOrEqual, Box::new(node), Box::new(self.add()));
             } else {
                 return node;
@@ -55,9 +66,9 @@ impl Parser {
         let mut node = self.mul();
 
         loop {
-            if self.expr.consume_reserved_token(ReservedToken::Add).unwrap() {
+            if self.program.consume_reserved_token(ReservedToken::Add).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Add, Box::new(node), Box::new(self.mul()));
-            } else if self.expr.consume_reserved_token(ReservedToken::Sub).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::Sub).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Sub, Box::new(node), Box::new(self.mul()));
             } else {
                 return node;
@@ -69,9 +80,9 @@ impl Parser {
         let mut node = self.primary();
 
         loop {
-            if self.expr.consume_reserved_token(ReservedToken::Mul).unwrap() {
+            if self.program.consume_reserved_token(ReservedToken::Mul).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Mul, Box::new(node), Box::new(self.primary()));
-            } else if self.expr.consume_reserved_token(ReservedToken::Div).unwrap() {
+            } else if self.program.consume_reserved_token(ReservedToken::Div).unwrap() {
                 node = Node::BinaryOperator(BinaryOperatorType::Div, Box::new(node), Box::new(self.primary()));
             } else {
                 return node;
@@ -80,12 +91,12 @@ impl Parser {
     }
 
     fn primary(&mut self) -> Node {
-        if self.expr.consume_reserved_token(ReservedToken::OpenBracket).unwrap() {
+        if self.program.consume_reserved_token(ReservedToken::OpenBracket).unwrap() {
             let node = self.add();
-            if !self.expr.consume_reserved_token(ReservedToken::CloseBracket).unwrap() { panic!(""); }
+            if !self.program.consume_reserved_token(ReservedToken::CloseBracket).unwrap() { panic!(""); }
             return node;
         }
 
-        Node::Number(self.expr.consume_number_token().unwrap())
+        Node::Number(self.program.consume_number_token().unwrap())
     }
 }
